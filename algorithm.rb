ee1222d4628c36be82ef48c403bf25c4e7cccd8a
sammilechman <sammilechman@gmail.com>
2015-01-26 12:05:21 -0800

    consolidate tests into seperate file

--- algorithm.rb
+++ algorithm.rb
@@ -49,24 +49,3 @@ def my_algorithm(arr)
   # don't need to keep running totals, return a 2-D array.
   sorted_arrays.map { |array| array.first }
 end
-
-def test_algorithm(input, expected)
-  puts "\nmy_algorithm(#{input}) should equal #{expected}."
-  if (my_algorithm(input) == expected)
-    puts "PASS"
-  else
-    puts "FAIL"
-    puts "Yields: #{my_algorithm(input)}"
-  end
-end
-
-def run_tests
-  puts "\n-- SORTING ALGORITHM --\n"
-  test_algorithm([3, 2, 5], [[5, 3, 2]])
-  test_algorithm([3, 2, 5, 5, 8], [[8, 3], [5, 5, 2]])
-  test_algorithm([8, 4, 2, 2, 2, 2, 2, 2],  [[8, 2, 2], [4, 2, 2, 2, 2]])
-  test_algorithm([9, 5, 3, 1], [[9], [5, 3, 1]])
-  puts "\n+++++++++++++++++++++++"
-end
-
-run_tests
