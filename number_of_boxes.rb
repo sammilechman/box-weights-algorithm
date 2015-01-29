53cf85da6737f9129162d130b5daea67cf38ad99
sammilechman <sammilechman@gmail.com>
2015-01-29 11:39:22 -0800

    number_of_boxes method passing all tests

--- number_of_boxes.rb
+++ number_of_boxes.rb
@@ -1,19 +1,27 @@
 # http://aaaipress.org/Papers/AAAI/2002/AAAI02-110.pdf
 
 require 'active_support/core_ext'
-require 'pry'
+# require 'pry'
 
 MAX_WEIGHT = 16.0
 
 def number_of_boxes(array)
   arr = array.sort.reverse
   boxes = [[0]]
-  missed_opportunity_space = 0
 
   until arr.empty?
-    box = boxes.first
-    box << arr.shift
-    space_left = MAX_WEIGHT - box.sum
+    space_left = calc_space_left(boxes[0])
+
+    if arr.first <= space_left
+      boxes[0] << arr.shift
+    elsif boxes[0].sum.zero? && arr.first > space_left
+      boxes[0] << space_left
+      arr.shift
+    else
+      boxes.unshift([arr.shift])
+    end
+
+    space_left = calc_space_left(boxes[0])
 
     total_secondary_adds = []
 
@@ -22,26 +30,32 @@ def number_of_boxes(array)
     end
 
     if total_secondary_adds.sum >= space_left
-      box << space_left
+      boxes[0] << space_left
       leftover_for_next_box = total_secondary_adds.sum - space_left
     else
-      box += total_secondary_adds
+      boxes[0] += total_secondary_adds
       leftover_for_next_box = 0
     end
 
-    missed_opportunity_space += (MAX_WEIGHT - box.sum)
-
     unless arr.empty? && leftover_for_next_box.zero?
-      boxes.unshift([leftover_for_next_box])
+      arr << leftover_for_next_box unless leftover_for_next_box.zero?
+      arr.sort!.reverse!
     end
   end
 
   missed_opportunity_space = 0
+
   boxes.each do |box|
-    missed_opportunity_space += MAX_WEIGHT - box.sum
+    missed_opportunity_space += calc_space_left(box)
   end
 
+  array.map! { |box| box > 16 ? 16 : box }
   ((missed_opportunity_space + array.sum) / MAX_WEIGHT).ceil
 end
 
-number_of_boxes([3,2,5])
+def calc_space_left(box)
+  space_left = MAX_WEIGHT - box.sum
+  space_left >= 0 ? space_left : 0
+end
+
+# p number_of_boxes([1, 4, 25, 10])
